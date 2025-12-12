99929ee89a89ac5076983630a56dbbbbbe587fc0
Ed-Miliband <dejafarpouran@outlook.com>
2025-12-12 14:01:20 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,8 @@
-class 
+class Person:
+  def __init__(self):
+    self.health = 100
+    self.power = 10
+    self.attack = 10
+    self.defense = 10
+    self.abilities = []
+    self.inventory = []
