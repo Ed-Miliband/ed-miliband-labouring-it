3d34df35d1567bb345592a6b62c087327dfa0822
Ed-Miliband <dejafarpouran@outlook.com>
2026-01-04 19:23:43 +0000

    Update MainMenu.py

--- Main Menu/MainMenu.py
+++ Main Menu/MainMenu.py
@@ -18,9 +18,17 @@ clock = pygame.time.Clock()
 #Main Program Loop 
 while not done:
     # --- Main event loop - Code for mouse keyboard + mouse clicks go here
-    for event in pygame.event.get(): #User did something
-        if event.type == pygame.QUIT: #If user clicked close
-            done = True #Exit this loop
+    for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        print("User asked to quit.")
+        pygame.quit()
+    elif event.type == pygame.KEYDOWN:
+        print("User pressed a key.")
+    elif event.type == pygame.KEYUP:
+        print("User let go of a key.")
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+        print("User pressed a mouse button")
+
  
     #Game logic
  #Code for objects etc.
