ab6d842ebe6016e2db01e36c26a0982e60845122
Ed-Miliband <dejafarpouran@outlook.com>
2026-01-04 19:19:53 +0000

    Update MainMenu.py

--- Main Menu/MainMenu.py
+++ Main Menu/MainMenu.py
@@ -10,3 +10,24 @@ BLUE     = (   0,   0, 255)
 
 size = (700, 500)
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Hallelujah")
+
+done = False
+clock = pygame.time.Clock()
+ 
+#Main Program Loop 
+while not done:
+    # --- Main event loop - Code for mouse keyboard + mouse clicks go here
+    for event in pygame.event.get(): #User did something
+        if event.type == pygame.QUIT: #If user clicked close
+            done = True #Exit this loop
+ 
+    #Game logic
+ #Code for objects etc.
+  
+    #Drawing code
+    screen.fill(WHITE) #Background - drawing comes after
+
+    pygame.display.flip() #"Update the full display Surface to the screen"
+ 
+    clock.tick(60) #Frame rate limit
