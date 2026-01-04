c8647da99baaf0e7d9b2aac80491d12342fdef96
Ed-Miliband <dejafarpouran@outlook.com>
2026-01-04 19:08:49 +0000

    Update MainMenu.py

--- Main Menu/MainMenu.py
+++ Main Menu/MainMenu.py
@@ -1,1 +1,12 @@
+import pygame
+pygame.init() #initialise pygame engine
 
+#Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+
+size = (700, 500)
+screen = pygame.display.set_mode(size)
