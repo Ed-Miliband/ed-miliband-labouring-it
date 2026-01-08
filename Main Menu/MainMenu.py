b8933b7b0c6fed2c1ccd32f6a6a9a41b51f0afca
Ed-Miliband <dejafarpouran@outlook.com>
2026-01-08 19:01:56 +0000

    Update MainMenu.py

--- Main Menu/MainMenu.py
+++ Main Menu/MainMenu.py
@@ -20,6 +20,26 @@ text_input="QUIT", font=get_font (75), base_color="#d7fcd4", hovering color="Whi
 
    SCREEN.blit (MENU_TEXT, MENU_RECT)
 
+  for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]: 
+    button.changeColor (MENU_MOUSE_POS)
+    button.update(SCREEN)
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      if PLAY_BUTTON. check For Input (MENU_MOUSE_POS): 
+        play()
+    if OPTIONS_BUTTON.checkForInput (MENU_MOUSE_POS):
+      options()
+    if QUIT_BUTTON.checkForInput (MENU_MOUSE_POS):
+      pygame.quit() sys.exit()
+      
+  pygame.display.update()
+
+main menu()
+
 def options(): 
    pygame.display.set_caption("Options")
 
