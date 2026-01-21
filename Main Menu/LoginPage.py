26aa7acd88f271286f8971b69724b853e39abc3c
Ed-Miliband <dejafarpouran@outlook.com>
2026-01-21 11:18:58 +0000

    dig in yo buh twin

--- Main Menu/LoginPage.py
+++ Main Menu/LoginPage.py
@@ -5,9 +5,9 @@ class Button():
     self.image image
     self.x_pos = pos[0]
     self.y_pos = pos[1]
-    self.font
-    font
-    self.base_color, self.hovering_color=base_color, hovering_color self.text_input = text_input
+    self.font = font
+    self.base_color, self.hovering_color = base_color, hovering_color 
+    self.text_input = text_input
     self.text = self.font.render(self.text_input, True, self.base_color) 
     if self.image is None:
       self.image self.text
