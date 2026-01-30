1136b2c6fe27e0f903489f2e6bb1c5b28af30923
Ed-Miliband <dejafarpouran@outlook.com>
2026-01-30 10:24:16 +0000

    Update SaveLoad.py

--- Saves/SaveLoad.py
+++ Saves/SaveLoad.py
@@ -1,3 +1,13 @@
 from Saves import SaveData
+import json
 
 print("bornana")
+
+#Utility functions for JSON
+def load_json(filepath):
+    with open(filepath, "r") as f:
+        return json.load(f)
+      
+def save_json(filepath, data):
+    with open(filepath, "a") as f:
+        json.dump(data, f, indent=4)
