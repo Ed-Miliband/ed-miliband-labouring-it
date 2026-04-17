7fd3e4a94034bc2b4fee05e6faca0f6e089b0dfc
Ed-Miliband <dejafarpouran@outlook.com>
2026-04-17 10:41:43 +0100

    Create utility.py

---
+++ utility.py
@@ -0,0 +1,8 @@
+#Utility functions for JSON
+def load_json(filepath):
+    with open(filepath, "r") as f:
+        return json.load(f)
+      
+def save_json(filepath, data):
+    with open(filepath, "a") as f:
+        json.dump(data, f, indent=4)
